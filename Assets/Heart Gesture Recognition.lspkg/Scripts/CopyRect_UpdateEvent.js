f187c04efc4cd974e4f4e5a4ceb4ba841aafccf3
DebabrataDas1 <dskdas7again@gmail.com>
2026-02-06 10:24:33 +0530

    With Glam

--- Assets/Heart Gesture Recognition.lspkg/Scripts/CopyRect_UpdateEvent.js
+++
@@ -1,2 +0,0 @@
-//@input Asset.Texture textureInput
-script.createEvent("UpdateEvent").bind(function() { require("CopyRect_wrapped")(script)})
--- Assets/Heart Gesture Recognition.lspkg/Scripts/Labels.js
+++
@@ -1,5 +0,0 @@
-script.labels = {
-    "0": "No_Heart",
-    "1": "Heart_1",
-    "2": "Heart_2"
-}
